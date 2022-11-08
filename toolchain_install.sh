3d37669b22ba8843b7c569663df4d23d792b2237
Sidharth Sahni <sidharthsahniii@gmail.com>
2022-11-09 01:16:46 +0530

    added stuff and changed order

--- toolchain_install.sh
+++ toolchain_install.sh
@@ -1,6 +1,5 @@
 ## brew install karlo phle
 
-
 echo wowstuff
 
 brew install node
@@ -11,6 +10,31 @@ brew install fish
 # rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+echo dev tools
+
+brew install --cask insomnia
+brew install --cask android-studio
+brew install --cask keycastr
+brew install --cask github
+brew install --cask warp
+brew install --cask visual-studio-code # kabhi kabhi need padti XD
+# kitty
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+# brew install --cask kitty
+# brew install --cask flipper
+
+echo random apps
+
+brew install --cask slack
+brew install --cask karabiner-elements # for changing keybindings
+brew install --cask spotify
+brew install --cask vlc
+brew install --cask obs
+
+echo github login
+
+brew install gh
+gh auth login
 
 echo neovim setup
 
@@ -19,28 +43,9 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone https://github.com/deevo-sage/nvim-config ~/.config/nvim
 
-echo dev-tools
-
-brew install --cask insomnia
-brew install --cask android-studio
-brew install --cask keycastr
-brew install --cask github
-brew install --cask warp
-brew install --cask visual-studio-code
-# kitty
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-# brew install --cask kitty
-# brew install --cask flipper
-
-echo random apps
-
-brew install --cask spotify
-brew install --cask karabiner-elements
-brew install --cask obs
-brew install --cask slack
-brew install --cask vlc
-
+# to start fish in zsh by default uncomment next line
+# echo "fish" >> ~/.zshrc
+#
 # fonts
 # https://www.nerdfonts.com/font-downloads
-# caskaydiacode, firacode
-
+# caskaydiacode(*), firacode
